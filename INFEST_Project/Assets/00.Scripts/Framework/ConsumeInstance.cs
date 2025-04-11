0ad172af4278a98285ec5b20f891ccd0717110e2
anbak98 <anbak98@gmail.com>
2025-04-11 17:15:08 +0900

    WIP

---
+++ INFEST_Project/Assets/00.Scripts/Framework/ConsumeInstance.cs
@@ -0,0 +1,6 @@
+using Unity.VisualScripting;
+
+public class ConsumeInstance
+{
+    public readonly ConsumeItem data;
+}
---
+++ INFEST_Project/Assets/00.Scripts/Framework/MonsterInstance.cs
@@ -0,0 +1,4 @@
+public class MonsterInstance
+{
+    public readonly MonsterInfo data;
+}
