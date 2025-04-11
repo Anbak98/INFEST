94621d35b61321974038973915611ba6657e056d
anbak98 <anbak98@gmail.com>
2025-04-11 14:49:56 +0900

    fix: syntax error

--- INFEST_Project/Assets/00.Scripts/Framework/Pool/PooledNetworkObjectProvider.cs
+++ INFEST_Project/Assets/00.Scripts/Framework/Pool/PooledNetworkObjectProvider.cs
@@ -1,44 +1,44 @@
-using Fusion;
-using System.Collections.Generic;
-using UnityEngine;
+//using Fusion;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PooledNetworkObjectProvider : NetworkObjectProviderDefault
-{
-    private Dictionary<NetworkPrefabId, BaseNetworkObjectPool<NetworkObject>> _pools = new();
+//public class PooledNetworkObjectProvider : NetworkObjectProviderDefault
+//{
+//    private Dictionary<NetworkPrefabId, BaseNetworkObjectPool<NetworkObject>> _pools = new();
 
-    public void RegisterPool(NetworkPrefabId prefabId, BaseNetworkObjectPool<NetworkObject> pool)
-    {
-        _pools[prefabId] = pool;
-    }
+//    public void RegisterPool(NetworkPrefabId prefabId, BaseNetworkObjectPool<NetworkObject> pool)
+//    {
+//        _pools[prefabId] = pool;
+//    }
 
-    protected override NetworkObject InstantiatePrefab(NetworkRunner runner, NetworkObject prefab)
-    {
-        var prefabId = prefab.;
+//    protected override NetworkObject InstantiatePrefab(NetworkRunner runner, NetworkObject prefab)
+//    {
+//        var prefabId = prefab.;
 
-        if (_pools.TryGetValue(prefabId, out var pool))
-        {
-            var pooled = pool.Get();
-            return pooled.obj;
-        }
+//        if (_pools.TryGetValue(prefabId, out var pool))
+//        {
+//            var pooled = pool.Get();
+//            return pooled.obj;
+//        }
 
-        // fallback to default behavior
-        return base.InstantiatePrefab(runner, prefab);
-    }
+//        // fallback to default behavior
+//        return base.InstantiatePrefab(runner, prefab);
+//    }
 
-    protected override void DestroyPrefabInstance(NetworkRunner runner, NetworkPrefabId prefabId, NetworkObject instance)
-    {
-        if (_pools.TryGetValue(prefabId, out var pool))
-        {
-            pool.Return(new NetworkPoolObject<NetworkObject>
-            {
-                obj = instance,
-                IsPooled = true,
-                index = -1
-            });
-        }
-        else
-        {
-            base.DestroyPrefabInstance(runner, prefabId, instance);
-        }
-    }
-}
+//    protected override void DestroyPrefabInstance(NetworkRunner runner, NetworkPrefabId prefabId, NetworkObject instance)
+//    {
+//        if (_pools.TryGetValue(prefabId, out var pool))
+//        {
+//            pool.Return(new NetworkPoolObject<NetworkObject>
+//            {
+//                obj = instance,
+//                IsPooled = true,
+//                index = -1
+//            });
+//        }
+//        else
+//        {
+//            base.DestroyPrefabInstance(runner, prefabId, instance);
+//        }
+//    }
+//}
