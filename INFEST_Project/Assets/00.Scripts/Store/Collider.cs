23c03f4bc6c4562ed92fe30602d95c8081f2414b
JungSuYoung <zkwm987@gmail.com>
2025-04-16 17:04:35 +0900

    feat: StoreUi Show

--- INFEST_Project/Assets/00.Scripts/Store/Collider.cs
+++ INFEST_Project/Assets/00.Scripts/Store/Collider.cs
@@ -1,46 +1,60 @@
+using System.Collections;
 using System.Collections.Generic;
 using Fusion;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Collider : MonoBehaviour
 {
     private readonly HashSet<NetworkObject> _inside = new ();
-    private Store _store;
 
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
         if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
-        var networkObj = other.GetComponent<NetworkObject>();
-        if (networkObj == null) return;
+        var _player = other.GetComponent<NetworkObject>();
+        if (_player == null) return;
 
-        _store = GetComponentInParent<Store>();
+        var _store = GetComponentInParent<Store>();
         _store.inZone = true;
 
-        if (!_inside.Contains(networkObj))
+        if (!_inside.Contains(_player))
         {
             Debug.Log("접촉");
-            _inside.Add(networkObj);
+            _inside.Add(_player);
             _store.EnterShopZone();
         }
-        else if(_inside.Contains(networkObj) && Input.GetKeyDown(KeyCode.F))
+        else if(_inside.Contains(_player) && Input.GetKeyDown(KeyCode.F))
         {
             Debug.Log("상호작용");
             _store.Interaction();
-            Debug.Log(_store.isInteraction);
         }
-        else if(_store.isInteraction && Input.GetKeyDown(KeyCode.X))
+        else if(_store.isInteraction && Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("상호작용 해제");
-            _inside.Remove(networkObj); 
+            _inside.Remove(_player); 
             _store.EndShopZone();
         }
 
         if (_store.storeTimer.ExpiredOrNotRunning(_store.Runner))
         {
-            Debug.Log("접촉 해제");
-            _inside.Remove(networkObj);
-            _store.EndShopZone();
+            Exit(_player, _store);
         }
+
+        StopAllCoroutines();
+        StartCoroutine(ICorutine(_player, _store));
+    }
+
+    IEnumerator ICorutine(NetworkObject _player, Store _store)
+    {
+        yield return new WaitForSeconds(3f);
+        Exit(_player, _store);
+    }
+
+    public void Exit(NetworkObject _player, Store _store)
+    {
+        Debug.Log("접촉 해제");
+        _inside.Remove(_player);
+        _store.EndShopZone();
     }
 }
