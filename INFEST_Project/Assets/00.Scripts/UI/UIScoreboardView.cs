142e0267bf7262182f4c45dd6234088af994539d
JungSuYoung <zkwm987@gmail.com>
2025-04-24 17:51:11 +0900

    wip

--- INFEST_Project/Assets/00.Scripts/UI/UIScoreboardView.cs
+++ INFEST_Project/Assets/00.Scripts/UI/UIScoreboardView.cs
@@ -43,7 +43,7 @@ public class UIScoreboardView : UIScreen
         {
             kills = 0,
             deaths = 0,
-            characterInfoInstance.curGold = 500
+            gold = 500
         };
         row.SetData(init);
 
