fe07c454db62e3c3e6891dceba23b4929c2f828c
anbak98 <anbak98@gmail.com>
2025-05-01 14:33:47 +0900

    wip

--- INFEST_Project/Assets/00.Scripts/Match/MVPStageSpawner.cs
+++ INFEST_Project/Assets/00.Scripts/Match/MVPStageSpawner.cs
@@ -98,7 +98,7 @@ public class MVPStageSpawner : MonoBehaviour, INetworkRunnerCallbacks
             _spawnedCharacters.Add(player, networkPlayerObject);
             if (runner.LocalPlayer == player)
             {
-                MonsterSpawner monsterSpawner = runner.Spawn(_monsterSpawnerPrefab, new Vector3(-20,10,0)).GetComponent<MonsterSpawner>();
+                MonsterSpawner monsterSpawner = runner.Spawn(_monsterSpawnerPrefab, Vector3.zero).GetComponent<MonsterSpawner>();
                 monsterSpawner.SpawnMonsterOnWave();
 
                 runner.Spawn(_storePrefab, Vector3.zero);
