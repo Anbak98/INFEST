ade8fc382aa8bd64557c1fd6c4cd3eb468cfc8a5
JungSuYoung <zkwm987@gmail.com>
2025-05-01 21:32:29 +0900

    wip

--- INFEST_Project/Assets/00.Scripts/Monster/WaveTrigger.cs
+++ INFEST_Project/Assets/00.Scripts/Monster/WaveTrigger.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class WaveTrigger : MonoBehaviour
 {
     [SerializeField] private MVPStageSpawner _MVPStageSpawner;
+    bool _isTrigger = false;
     private int _playerLayer = 7;
 
     private void OnTriggerEnter(UnityEngine.Collider other)
@@ -11,11 +12,11 @@ public class WaveTrigger : MonoBehaviour
 
         if (other.gameObject.layer == _playerLayer && player)
         {
+            if (_isTrigger) return;
             if (_MVPStageSpawner.monsterSpawner == null) return;
 
-            _MVPStageSpawner.monsterSpawner.RPC_WaveStart();
-            enabled = false;
-
+            _MVPStageSpawner.monsterSpawner.SpawnMonsterOnWave();
+            _isTrigger = true;
         }
     }
 }
