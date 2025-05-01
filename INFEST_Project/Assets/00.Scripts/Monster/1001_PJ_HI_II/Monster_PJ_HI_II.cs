4b0cfd342ceee9cd8e52d4e0346995e34cf58613
Jujeongmin <anjshdkdl99@gmail.com>
2025-05-01 19:34:00 +0900

    feat: PJ_HI II Add

---
+++ INFEST_Project/Assets/00.Scripts/Monster/1001_PJ_HI_II/Monster_PJ_HI_II.cs
@@ -0,0 +1,19 @@
+public class Monster_PJ_HI_II : MonsterNetworkBehaviour
+
+{
+    public override void Render()
+    {
+        animator.SetFloat("MovementSpeed", MovementSpeed);
+        if (IsAttack)
+        {
+            animator.SetTrigger("IsAttack");
+        }
+        if (IsDead)
+        {
+            FSM.ChangePhase<PJ_HI_DeadPhase>();
+            animator.SetBool("IsDead", IsDead);
+        }
+
+        AIPathing.speed = MovementSpeed;
+    }
+}
