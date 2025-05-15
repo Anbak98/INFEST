193e5c3a8ae3465d9deba817bc5a29207108e6c5
Kim Seong Won <worni5015@gmail.com>
2025-05-15 12:14:00 +0900

    feat: Add Data Loader, Json (엘리트 몬스터 스킬테이블, 스폰 테이블)

---
+++ INFEST_Project/Util/ExcelToJsonWizard.v1.0.6/loader_output/GritaSkillTable.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class GritaSkillTable : IKeyedItem
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    public int key;
+
+    /// <summary>
+    /// 이름
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// 데미지 계수
+    /// </summary>
+    public float DamageCoefficient;
+
+    /// <summary>
+    /// 쿨다운
+    /// </summary>
+    public float CoolDown;
+
+    /// <summary>
+    /// 스킬 사용 범위
+    /// </summary>
+    public float UseRange;
+
+    /// <summary>
+    /// 웨이브 시 사용 범위
+    /// </summary>
+    public float UseRangeInWave;
+
+    /// <summary>
+    /// 사용 우선순위
+    /// </summary>
+    public int SkillPriority;
+
+    public int Key => key;
+
+}
