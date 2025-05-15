eb579acdf6a08f027ca765353ad07113ed35d270
Imperor <imperor0103@gmail.com>
2025-05-15 10:32:51 +0900

    wip: monster animation name

--- INFEST_Project/Assets/00.Scripts/Monster/2001_Grita/Grita_Dead_Dead.cs
+++ INFEST_Project/Assets/00.Scripts/Monster/2001_Grita/Grita_Dead_Dead.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 
 public class Grita_Dead_Dead : MonsterStateNetworkBehaviour<Monster_Grita, Grita_Phase_Dead>
 {
-    public TickTimer _tickTimer;
-    public NetworkObject obj;
+    //public TickTimer _tickTimer;
+    //public NetworkObject obj;
 
-    public override void Enter()
-    {
-        base.Enter();
-        monster.CurMovementSpeed = 0f;
-        _tickTimer = TickTimer.CreateFromSeconds(Runner, 7);
+    //public override void Enter()
+    //{
+    //    base.Enter();
+    //    monster.CurMovementSpeed = 0f;
+    //    _tickTimer = TickTimer.CreateFromSeconds(Runner, 7);
 
-    }
-    public override void Execute()
-    {
-        base.Execute();
+    //}
+    //public override void Execute()
+    //{
+    //    base.Execute();
 
-        if (_tickTimer.Expired(Runner))
-        {
-            if (HasStateAuthority)
-            {
-                Runner.Despawn(obj);
-            }
-        }
-    }
+    //    if (_tickTimer.Expired(Runner))
+    //    {
+    //        if (HasStateAuthority)
+    //        {
+    //            Runner.Despawn(obj);
+    //        }
+    //    }
+    //}
 }
