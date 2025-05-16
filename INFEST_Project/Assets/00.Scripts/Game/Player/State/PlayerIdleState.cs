a220c03b475ba25fee1890c9fbdda9d93b42d8ec
JungSuYoung <zkwm987@gmail.com>
2025-05-16 11:55:11 +0900

    Reapply "feat: Reload"
    This reverts commit 6bb3ef5f891c7c259724dba32427a067799c740c.

--- INFEST_Project/Assets/00.Scripts/Game/Player/State/PlayerIdleState.cs
+++ INFEST_Project/Assets/00.Scripts/Game/Player/State/PlayerIdleState.cs
@@ -64,6 +64,11 @@ public class PlayerIdleState : PlayerGroundState
             stateMachine.ChangeState(stateMachine.AimState);
         }
 
+        //if(stateMachine.Player.Weapons != null && data.isReloading)
+        //{
+        //    stateMachine.ChangeState(stateMachine.ReloadState);
+        //}
+
 
         // 앉는 상태는 잠금
         //if (controller.IsGrounded() && data.isSitting)
